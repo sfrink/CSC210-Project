3fa75b1e9f161d3bafcdd2d99c635a60145bf1ac
kbartlet <kbartlet@u.rochester.edu>
2013-04-28 23:40:43 +0000

    added a few template files that ill continue to work on

---
+++ home.php
@@ -0,0 +1,20 @@
+<!-- This file is just a start -Keenan -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Home</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link href="home.css" type="text/css" rel="stylesheet" />
+</head>
+
+<?php
+
+//this would be our file that holds all the php functions for querying
+//from our database. This file's name could be different if you want
+include sqlquery.php;
+
+?>
+
+</html>
