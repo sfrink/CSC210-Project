a3cb7ab7572b3600a73078b9c2f393e906c9b7fe
kbartlet <kbartlet@u.rochester.edu>
2013-05-06 16:48:50 -0300

    added home page with most of the necessary functionality

--- home.php
+++ home.php
@@ -1,23 +1,72 @@
-<!-- This file is just a start -Keenan -->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 
 <head>
-	<title>Home</title>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<link href="home.css" type="text/css" rel="stylesheet" />
-	<?php include 'queries.php'; ?>
+<title>Home</title>
+<meta charset="utf-8">
+<link href="home.css" type="text/css" rel="stylesheet" />
 </head>
 
 <body>
-<?php 
-	//echo getBandName("cool"); 
-	$event_list = getEventList();
-	for ($i = 0; $i < (sizeof($event_list)); $i++) {
-		echo $event_list[$i];
-	}	
-?>
+
+<?php include 'queries.php'; ?>
+
+<div id="banner">
+	<div id="banner_text">
+		Roc 'N' Roll
+	</div>
+</div>
+
+<nav>
+	<a href="home.php">Home</a>
+	<a href="about.html">About</a>
+	<a href="faq.html">FAQ</a>
+	<a href="browse.php">Browse Bands</a>
+	<a href="band.php">My Band</a>
+	<a href="signin.php">Sign In / Sign up</a>
+</nav>
+
+<div id="content_frame">
+	<h2> All Events </h2>
+	<div id="content">
+		<?php
+			$event_list = getEventListInOrder();
+			foreach ($event_list as $event_id) {
+				/*TODO If this is a new date from the event before, make a date banner*/
+				/*event info row*/
+				echo getRow($event_id);
+			}
+		?>
+	</div>
+</div>
+
 </body>
+
+<!--FUNCTIONS-->
+<?php
+	function getRow($event_id) {
+		ob_start();
+		?>
+		<html>
+			<div id="date_banner">
+				<?php echo getEventDate($event_id); ?>
+			</div>
+			<div id="row">
+				<?php 
+					$band_id = getEventBand($event_id);
+					$img_url = getBandPic($band_id);
+					$band_name = getBandName($band_id);
+					echo getEventTime($event_id)
+				?>
+				<img src= "http://www.waughsd.org/fxconsult1/userfiles/band(1).jpg" width="50" height="50" >
+				<?php echo $band_name ?> at <?php echo getEventLocation($event_id); ?>
+				<?php echo getEventPrice($event_id); ?>
+			</div>
+		</html>
+		<?php
+		$row = ob_get_clean();
+		return $row;
+	}
+?>
 
 </html>
