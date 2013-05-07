70ee115048b8631f306ea44ef58eaf1eeb67045f
Steven Frink <sfrink1@gmail.com>
2013-05-07 19:51:32 +0000

    fixed some stuff

--- about.php
+++ about.php
@@ -9,7 +9,27 @@
 
 <body>
 
-	<?php include 'header.php'; ?>
+<div id="banner">
+<div id="banner_text">
+Roc 'N' Roll
+</div>
+</div>
+
+<nav>
+<a href="home.php">Home</a>
+<a href="about.php">About</a>
+<a href="faq.php">FAQ</a>
+<a href="browsebands.php">Browse Bands</a>
+<a href="band.php">My Band</a>
+<?php
+	if(isset($_COOKIE["user"])){
+		print "<a href=\"logout.php\">Log out</a>";
+	}
+	else{
+		print "<a href=\"signin.php\">Sign In / Sign up</a>";
+	}
+	?>
+</nav>
 	
 	<div id="content_frame">
 	<h2>About</h2>
