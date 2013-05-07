53aca5bbf7e11b3e6bae458f30be64e206e7d33e
kbartlet <kbartlet@u.rochester.edu>
2013-05-07 12:58:55 -0300

    ignores past events. date banners working better

--- home.php
+++ home.php
@@ -9,7 +9,8 @@
 
 <body>
 
-<?php 
+<?php
+	date_default_timezone_set('America/New_York');
 	include 'queries.php';
 	include 'header.php';
 ?>
@@ -19,11 +20,24 @@
 	<div id="content">
 		<?php
 			$event_list = getEventListInOrder();
-			foreach ($event_list as $event_id) {
-				/*TODO If this is a new date from the event before, make a date banner*/
-				/*event info row*/
+			for ($i=0; $i < sizeof($event_list); $i++) {
+				$event_id = $event_list[$i];
+				
+				if (eventDateHasPassed($event_id)) {continue;}
+						
+				//print date banner
+				if ($i == 0) { //always print first date banner
+					echo getDateBanner($event_id);
+				} else if (getEventDate($event_id) != getEventDate($event_list[$i-1])) {
+					echo getDateBanner($event_id);
+				} else {
+					echo "<hr>";
+				}
+				
+				//print row
 				echo getRow($event_id);
 			}
+			echo "<hr>";
 		?>
 	</div>
 </div>
@@ -32,13 +46,11 @@
 
 <!--FUNCTIONS-->
 <?php
+	//this method still needs work
 	function getRow($event_id) {
 		ob_start();
 		?>
 		<html>
-			<div id="date_banner">
-				<?php echo getEventDate($event_id); ?>
-			</div>
 			<div id="row">
 				<?php 
 					$band_id = getEventBand($event_id);
@@ -55,6 +67,30 @@
 		$row = ob_get_clean();
 		return $row;
 	}
+?>
+
+<?php
+	function getDateBanner($event_id) {
+		ob_start();
+		?>
+		<html>
+			<div id="date_banner">
+				<?php echo getEventDate($event_id); ?>
+			</div>
+		</html>
+		<?php
+		$date_banner = ob_get_clean();
+		return $date_banner;
+	}
+?>
+
+<?php
+	function eventDateHasPassed($event_id) {
+		$event_date = getEventDate($event_id);
+		$cur_date = date('Y-m-d');
+		if ($event_date < $cur_date) { return TRUE;}
+		else {return FALSE;}
+	}	
 ?>
 
 </html>
